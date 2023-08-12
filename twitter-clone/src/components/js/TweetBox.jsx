756f895aa888a733b5c706042ac5c75b81679232
ethkav03 <ethkav03@gmail.com>
2023-08-12 22:54:32 +0100

    20230812_2253 - Added Tweet box (non-functional)

--- twitter-clone/src/components/js/TweetBox.jsx
+++ twitter-clone/src/components/js/TweetBox.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import "../css/TweetBox.css";
+import { Avatar, Button } from "@mui/material";
 
 function TweetBox() {
   return (
-    <div className='TweetBox'>
-        <h1>TweetBox</h1>
+    <div className='tweetBox'>
+        <form action="">
+          <div className="tweetBox_input">
+            <Avatar src="https://lh3.googleusercontent.com/ogw/AGvuzYYGaRMJjz_0xWuNAr5Q9hZiCRrBR73YEwzCwooN=s32-c-mo" />
+            <input type="text" placeholder="What's happening?"  />
+          </div>
+          <input type="text" placeholder='Enter image URL' className='tweetBox_imageInput' />
+          <Button className='tweetBox_tweetButton'>Tweet</Button>
+        </form>
     </div>
   )
 }
